b6c88bce504224c716d9d80613bad6c1e12348a8
DelahayesTheo <delahayest@gmail.com>
2018-08-18 18:29:20 +0200

    PowerUpManager

---
+++ Assets/Scripts/DuckControllerT.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class DuckControllerT : MonoBehaviour
+{
+    public float walkingSpeed;
+
+    Rigidbody2D m_Rigidbody2D;
+
+    void Start()
+    {
+
+        m_Rigidbody2D = transform.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+        m_Rigidbody2D.velocity = new Vector2(h * walkingSpeed, v * walkingSpeed);
+    }
+
+    public void PowerUp(string name)
+    {
+        Debug.Log(name);
+    }
+}
