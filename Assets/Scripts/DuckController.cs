6275b63f466a4bba98aa917a66eac184c6333e8b
Alshak <alain.shakour@gmail.com>
2018-08-18 23:35:15 +0200

    Duck

--- Assets/Scripts/DuckController.cs
+++ Assets/Scripts/DuckController.cs
@@ -29,13 +29,18 @@ public class DuckController : MonoBehaviour
         bool dash = CrossPlatformInputManager.GetButtonDown(GetControl(numPlayer, "Dash"));
         if (dash && m_CurrentDashCooldown <= 0f)
         {
-            m_CurrentDashDuration = dashDuration;
-            m_CurrentDashCooldown = dashCooldown;
-            m_Rigidbody2D.velocity = Vector2.zero;
-            m_Rigidbody2D.AddForce(lastVelocity.normalized * dashSpeed);
+            Dash(lastVelocity.normalized);
             dashParticles.transform.rotation = Quaternion.LookRotation(-lastVelocity.normalized);
             dashParticles.Play();
         }
+    }
+
+    public void Dash(Vector2 direction)
+    {
+        m_CurrentDashDuration = dashDuration;
+        m_CurrentDashCooldown = dashCooldown;
+        m_Rigidbody2D.velocity = Vector2.zero;
+        m_Rigidbody2D.AddForce(direction * dashSpeed);
     }
 
     void FixedUpdate()
@@ -59,24 +64,36 @@ public class DuckController : MonoBehaviour
         if (m_CurrentDashCooldown > 0f)
         {
             m_CurrentDashCooldown -= Time.deltaTime;
-            m_Rigidbody2D.velocity = Vector2.Lerp(m_Rigidbody2D.velocity, Vector2.zero, Time.deltaTime * (1 / dashCooldown));
+            m_Rigidbody2D.velocity = Vector2.Lerp(m_Rigidbody2D.velocity, Vector2.zero, Time.deltaTime * 2);
             return;
         }
 
-        // Actual movement
-        m_Rigidbody2D.velocity = new Vector2(h * walkingSpeed, v * walkingSpeed);
         m_Animator.SetFloat("horizontal", h);
         m_Animator.SetFloat("vertical", v);
 
         // Save last velocity
         if (!Mathf.Approximately(h, 0f) || !Mathf.Approximately(v, 0f))
         {
+            // Actual movement
+            m_Rigidbody2D.velocity = new Vector2(h * walkingSpeed, v * walkingSpeed);
             lastVelocity = new Vector2(h, v);
         }
     }
 
     private string GetControl(int numPlayer, string action)
     {
-        return action + numPlayer;
+        return action + (numPlayer > 0 ? numPlayer : 1);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(m_CurrentDashDuration <= 0f)
+            {
+//                Vector2 otherPlayerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+//                collision.gameObject.GetComponent<DuckController>().Dash(otherPlayerVelocity);
+            }
+        }
     }
 }
