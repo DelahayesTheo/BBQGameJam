759b06c0efe53d51e2c51c30ebc9dac47f6c9600
DelahayesTheo <delahayest@gmail.com>
2018-08-18 20:39:34 +0200

    VOILA

--- Assets/Scripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpManager.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpManager : MonoBehaviour {
-
-    private DuckControllerT playerController;
-	// Use this for initialization
-	void Start () {
-        playerController = transform.parent.gameObject.GetComponent<DuckControllerT>();
+    private DuckController playerController;
+    // Use this for initialization
+    void Start()
+    {
+        playerController = transform.parent.gameObject.GetComponent<DuckController>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        playerController.PowerUp(collision.gameObject.name);
     }
 }
