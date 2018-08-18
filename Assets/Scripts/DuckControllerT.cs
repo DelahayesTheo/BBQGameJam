1e3b9d0b17bf460c0f58a772ab2dcc7f7a4e8d64
François DELAHAYES <delahayest@gmail.com>
2018-08-18 20:28:51 +0200

    blablabla

--- Assets/Scripts/DuckControllerT.cs
+++ Assets/Scripts/DuckControllerT.cs
@@ -4,12 +4,14 @@ using UnityStandardAssets.CrossPlatformInput;
 public class DuckControllerT : MonoBehaviour
 {
     public float walkingSpeed;
+    public float speedBoost;
 
+    public bool isFaster;
     Rigidbody2D m_Rigidbody2D;
 
     void Start()
     {
-
+        isFaster = false;
         m_Rigidbody2D = transform.GetComponent<Rigidbody2D>();
     }
 
@@ -18,10 +20,21 @@ public class DuckControllerT : MonoBehaviour
         float h = CrossPlatformInputManager.GetAxis("Horizontal");
         float v = CrossPlatformInputManager.GetAxis("Vertical");
         m_Rigidbody2D.velocity = new Vector2(h * walkingSpeed, v * walkingSpeed);
+        if (isFaster){
+            m_Rigidbody2D.velocity = new Vector2(h * walkingSpeed * speedBoost, v * walkingSpeed * speedBoost);
+            SleepTimeout * 10;
+        }
+        else
+        {
+            m_Rigidbody2D.velocity = new Vector2(h * walkingSpeed, v * walkingSpeed);
+          
+        }
     }
 
     public void PowerUp(string name)
     {
-        Debug.Log(name);
+        if (name == "SpeedBoost"){
+            isFaster = true;
+        }
     }
 }
