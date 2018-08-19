ad32e3e980b08685855c8001913395fc63a668f8
DelahayesTheo <delahayest@gmail.com>
2018-08-19 03:48:27 +0200

    Yes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+    private Text textBox;
+    private Canvas canvas;
+	// Use this for initialization
+	void Start () {
+        textBox = GetComponentInChildren<Text>();
+        Time.timeScale = 0;
+
+        StartCoroutine(WaitToResumeGame());
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator WaitForCountdown() 
+    {
+        float start = Time.realtimeSinceStartup;
+        while (Time.realtimeSinceStartup < start + 1f) {
+            yield return 0;
+        }
+    }
+
+    IEnumerator WaitToResumeGame()
+    {
+        textBox.text = "3";
+        yield return WaitForCountdown();
+
+        textBox.text = "2";
+        yield return WaitForCountdown();
+
+        textBox.text = "1";
+        yield return WaitForCountdown();
+
+        textBox.text = "";
+        Time.timeScale = 1;
+    }
+}
